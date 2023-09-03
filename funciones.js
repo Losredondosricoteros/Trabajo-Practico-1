f90ff53558e19edb4e6713427906648fe50a9ae5
LordRider38 <jmf11.cordoba@gmail.com>
2023-09-03 19:00:09 -0300

    trabajo practico1

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function mostrar(){
+    document.getElementById("barra_menu") .classList.add("open")
+}
+
+function ocultar(){
+    document.getElementById("barra_menu") .classList.remove("open")
+}
